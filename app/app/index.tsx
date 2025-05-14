18aef194c6aab38f0ca359a665d03bc864e0135d
Eshank Tyagi <mreshanktyagi@gmail.com>
2025-05-14 07:04:13 +0530

    . . .

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,10 +1,16 @@
-import { WebView } from 'react-native-webview';
-import { StyleSheet, View, StatusBar } from 'react-native';
-import { useRef } from 'react';
+import { WebView } from "react-native-webview";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { useEffect, useRef } from "react";
 
 export default function App() {
   const webViewRef = useRef<WebView>(null);
-  const PRODUCTION_URL = 'https://lawexpert.vercel.app/';
+  const PRODUCTION_URL = "https://lawexpert.vercel.app/";
 
   const handleNavigationStateChange = (navState: any) => {
     // If the URL is not the root URL, navigate back to root
@@ -16,28 +22,39 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
-      <WebView
-        ref={webViewRef}
-        source={{ uri: PRODUCTION_URL }}
-        style={styles.webView}
-        onNavigationStateChange={handleNavigationStateChange}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        scalesPageToFit={true}
-        bounces={false}
-        allowsBackForwardNavigationGestures={false}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.webViewContainer}>
+        <StatusBar
+          translucent
+          backgroundColor="transparent"
+          barStyle="dark-content"
+        />
+
+        <WebView
+          ref={webViewRef}
+          source={{ uri: PRODUCTION_URL }}
+          style={styles.webView}
+          onNavigationStateChange={handleNavigationStateChange}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          scalesPageToFit={true}
+          bounces={false}
+          allowsBackForwardNavigationGestures={false}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+  },
+  webViewContainer: {
+    flex: 1,
+    marginTop: Platform.OS === "android" ? 28 : 0,
   },
   webView: {
     flex: 1,
