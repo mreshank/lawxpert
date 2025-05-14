d35ac051560267cbb6c00cb5b58f1bcc9c52b2dc
Eshank Tyagi <mreshanktyagi@gmail.com>
2025-05-14 10:09:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['https://lawexpert.vercel.app', 'http://localhost:5173'],
+  origin: ['https://lawexpert.vercel.app', 'http://localhost:5173', "*"],
   credentials: true
 }));
 app.use(express.json());
