366b41e55f63b19c6389b7480f0afaaff6572af6
Eshank Tyagi <mreshanktyagi@gmail.com>
2025-05-14 07:55:33 +0530

    removal

--- app-old1/app/index.js
+++
@@ -1,7 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // This file is needed for the Expo Router to work with SDK 53
-  // It simply redirects to our main app component
-  return <Redirect href="/home" />;
-} 
